cf2807d75521a1cdecd1644d2bf0f5e98e3899ce
Filip Budiša <filip.budisa@gmail.com>
2019-06-02 00:10:38 +0200

    Added sprint maven plugin and app main method

--- src/main/java/com/filipbudisa/kusur/App.java
+++ src/main/java/com/filipbudisa/kusur/App.java
@@ -1,4 +1,17 @@
 package com.filipbudisa.kusur;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories("com.filipbudisa.kusur.repository")
+@EntityScan("com.filipbudisa.kusur.model")
+@SpringBootApplication
 public class App {
+
+	public static void main(String[] args){
+		SpringApplication.run(App.class, args);
+	}
 }
